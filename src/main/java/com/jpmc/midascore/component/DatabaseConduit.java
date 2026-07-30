c82ac1932da603581534e944b1c79e44ee6a1ef8
Shreya Soni <sonishreya311@gmail.com>
2026-07-30 23:33:43 +0530

    Implement H2 transaction processing

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,43 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
+    private final TransactionRecordRepository transactionRecordRepository;
 
-    public DatabaseConduit(UserRepository userRepository) {
+    public DatabaseConduit(UserRepository userRepository, TransactionRecordRepository transactionRecordRepository) {
         this.userRepository = userRepository;
+        this.transactionRecordRepository = transactionRecordRepository;
     }
 
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
+    }
+
+    @Transactional
+    public void processTransaction(Transaction transaction) {
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+        float amount = transaction.getAmount();
+
+        if (sender == null || recipient == null || amount < 0 || sender.getBalance() < amount) {
+            return;
+        }
+
+        sender.setBalance(sender.getBalance() - amount);
+        recipient.setBalance(recipient.getBalance() + amount);
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+        transactionRecordRepository.save(new TransactionRecord(sender, recipient, amount));
     }
 
 }
--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -9,9 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionListener {
     private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
 
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
     public void receive(Transaction transaction) {
+        databaseConduit.processTransaction(transaction);
         logger.info("Received transaction: {}", transaction);
     }
 }
